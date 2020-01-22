c94334c540fd39c1e5bc8c74f6bdb8b4721f7b56
eichmeierbr <eichmeierbr@gmail.com>
2020-01-22 09:36:00 -0500

    Removed build and Devel folders

--- catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/eichmeierbr/cmu/aacas/catkin_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/eichmeierbr/mrsd/tasks/5/three/devel;/home/eichmeierbr/mrsd/tasks/5/two/devel;/home/eichmeierbr/Documents/mrob/devel;/opt/ros/kinetic".split(';') if "/home/eichmeierbr/mrsd/tasks/5/three/devel;/home/eichmeierbr/mrsd/tasks/5/two/devel;/home/eichmeierbr/Documents/mrob/devel;/opt/ros/kinetic" != "" else []
