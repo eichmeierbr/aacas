55750d701be816dc639a6f6bb87620b58a42a3ad
Stefan Zhu <zhuzheyao7@gmail.com>
2020-03-01 12:23:29 -0500

    crop pointcloud working

--- catkin_ws/src/lidar_process/src/get_depth.cpp
+++ catkin_ws/src/lidar_process/src/get_depth.cpp
@@ -1,16 +1,45 @@
-  #include <ros/ros.h>
-  // PCL specific includes
-  #include <sensor_msgs/PointCloud2.h>
-  #include <pcl_conversions/pcl_conversions.h>
-  #include <pcl/point_cloud.h>
-  #include <pcl/point_types.h>
-  #include <pcl/ModelCoefficients.h>
-  #include <pcl/filters/project_inliers.h>
-  #include<iostream> 
-  using namespace std;
-  
-  ros::Publisher pub;
-  
+#include <ros/ros.h>
+// PCL specific includes
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/filters/crop_box.h>
+#include <pcl/filters/project_inliers.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include<iostream> 
+using namespace std;
+
+ros::Publisher pub;
+
+
+
+// class lidar_process{
+//     private: 
+//     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_projected (new pcl::PointCloud<pcl::PointXYZ>);
+//     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+//     pcl::PointCloud<pcl::PointXYZ>::Ptr bodyFiltered (new pcl::PointCloud<pcl::PointXYZ>);
+//     ros::Subscriber sub
+    
+//     public: 
+//     lidar_process(ros::NodeHandle *n){
+//         sub = n->subscribe ("/velodyne_points", 1, cloud_cb);
+//     }
+
+//     void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& msg){
+        
+//     }
+
+
+// }
+
+
+
+
+
+
+//   pcl::visualization::PCLVisualizer viewer("Cloud Viewer");
   
   void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& msg)
   {
@@ -22,8 +51,11 @@
     // pcl_conversions::toPCL(*msg, *cloud);
 
 
+
+
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_projected (new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr bodyFiltered (new pcl::PointCloud<pcl::PointXYZ>);
     // pcl::PointCloud<pcl::PointXYZ> cloud;
     pcl::fromROSMsg (*msg, *cloud);
 
@@ -33,15 +65,25 @@
     //     cout << "y: " <<it -> y << endl;
     //     cout << "z: " <<it-> z << endl;  
     // }
+    // Define min and max for X, Y and Z
+    float minX = -0.2, minY = -0.5, minZ = -2.5;
+    float maxX = +0.2, maxY = +0.5, maxZ = +2.5;
+
+    pcl::CropBox<pcl::PointXYZ> boxFilter;
+    boxFilter.setMin(Eigen::Vector4f(minX, minY, minZ, 1.0));
+    boxFilter.setMax(Eigen::Vector4f(maxX, maxY, maxZ, 1.0));
+    boxFilter.setInputCloud(cloud);
+    boxFilter.filter(*bodyFiltered);
+
+    // viewer.addPointCloud (bodyFiltered,"body");
+    // viewer.spin();
 
 
-    print('testings')
-
-    for (std::size_t i = 0; i < cloud->points.size (); ++i)
+    for (std::size_t i = 0; i < bodyFiltered->points.size (); ++i)
     {
-        cout << "x:"<<cloud->points[i].x << endl;
-        cout << "y:"<<cloud->points[i].y << endl;
-        cout <<"z:"<< cloud->points[i].z << endl;
+        cout << "x:"<<bodyFiltered->points[i].x << endl;
+        // cout << "y:"<x<bodyFiltered->points[i].y << endl;
+        // cout <<"z:"<< bodyFiltered->points[i].z << endl;
     }
 
     // Create a set of planar coefficients with X=Y=0,Z=1
@@ -86,8 +128,8 @@
     transform_2.rotate (Eigen::AngleAxisf (theta, Eigen::Vector3f::UnitZ()));
 
     // Print the transformation
-    printf ("\nMethod #2: using an Affine3f\n");
-    std::cout << transform_2.matrix() << std::endl;
+    // printf ("\nMethod #2: using an Affine3f\n");
+    // std::cout << transform_2.matrix() << std::endl;
 
 
 
