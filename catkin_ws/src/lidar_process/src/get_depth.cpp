2c559c4a1d4fc980a45b30bc4943153cb6e0bd53
Stefan Zhu <zhuzheyao7@gmail.com>
2020-03-16 13:54:21 -0400

    new progress

--- catkin_ws/src/lidar_process/src/get_depth.cpp
+++ catkin_ws/src/lidar_process/src/get_depth.cpp
@@ -47,8 +47,8 @@ using namespace std;
 
 //     void crop_point_cloud(){
 //         float minX = -0.2, minY = -0.5, minZ = -2.5;
-//         float maxX = +0.2, maxY = +0.5, maxZ = +2.5;
 //         boxFilter.setMin(Eigen::Vector4f(minX, minY, minZ, 1.0));
+//         float maxX = +0.2, maxY = +0.5, maxZ = +2.5;
 //         boxFilter.setMax(Eigen::Vector4f(maxX, maxY, maxZ, 1.0));
 //         boxFilter.setInputCloud(cloud);
 //         boxFilter.filter(*bodyFiltered);
@@ -108,43 +108,69 @@ sensor_msgs::PointCloud2 pub_cloud;
     boxFilter.setInputCloud(cloud);
     boxFilter.filter(*bodyFiltered);
 
-    // pcl::PointCloud<pcl::PointXYZ>::Ptr pub_msg (new pcl::PointCloud<pcl::PointXYZ>);  
-    // msg->header.frame_id = "some_tf_frame";
-    // msg->height = msg->width = 1;
-    // msg->points.push_back(*bodyFiltered);
 
-    pcl::toROSMsg(*bodyFiltered,pub_cloud);
-    // pub_msg = bodyFiltered;
+    // pcl::toROSMsg(*bodyFiltered,pub_cloud);
 
-    // viewer.addPointCloud (bodyFiltered,"body");
-    // viewer.spin();
-
-    // pcl::visualization::PCLVisualizer viewer("Cloud Viewer");
-
-
-    for (std::size_t i = 0; i < bodyFiltered->points.size (); ++i)
-    {
-        cout << "x:"<<bodyFiltered->points[i].x << endl;
-        // cout << "y:"<x<bodyFiltered->points[i].y << endl;
-        // cout <<"z:"<< bodyFiltered->points[i].z << endl;
-    }
+    // for (std::size_t i = 0; i < bodyFiltered->points.size (); ++i)
+    // {
+    //     cout << "x:"<<bodyFiltered->points[i].x << endl;
+    //     // cout << "y:"<x<bodyFiltered->points[i].y << endl;
+    //     // cout <<"z:"<< bodyFiltered->points[i].z << endl;
+    // }
 
 
         /*  METHOD #2: Using a Affine3f
         This method is easier and less error prone
     */
     float theta = M_PI/4;
-    Eigen::Affine3f transform_2 = Eigen::Affine3f::Identity();
+    // Eigen::Affine3f extrinsics = Eigen::Affine3f::Identity();
+    Eigen::Matrix4f projection_matrix = Eigen::Matrix4f::Identity();
+    // projection_matrix <<384.46966552734375, 0.0, 314.06256103515625, 0.0, 
+    //             0.0, 384.46966552734375, 248.62062072753906, 0.0, 
+    //             0.0, 0.0, 1.0, 0.0,
+    //             0,0,0,1;
+
+    projection_matrix << 612.8899536132812, 0.0, 313.19580078125, 0.0, 
+                        0.0, 613.1091918945312, 248.6840362548828, 0.0, 
+                        0.0, 0.0, 1.0, 0.0, 
+                        0,0,0,1;
+    
+
+    // Eigen::Matrix4f intrinsics = Eigen::Matrix4f::Identity();  
+
+    // intrinsics <<   0.38446966552734375, 0, 0.31406256103515625, 0,
+    //                 0, 0.38446966552734375, 0.24862062072753906,0,
+    //                 0, 0, 1, 0,
+    //                 0,0,0,1;
+
+    // Eigen::Matrix4f camerat_matrix = intrinsics * extrinsics;
+
+
+
+
 
     // Define a translation of 2.5 meters on the x axis.
-    transform_2.translation() << 2.5, 0.0, 0.0;
+    // extrinsics.translation() << 2.5, 0.0, 5;
 
-    // The same rotation matrix as before; theta radians around Z axis
-    transform_2.rotate (Eigen::AngleAxisf (theta, Eigen::Vector3f::UnitZ()));
+    // // The same rotation matrix as before; theta ` 56radians around Z axis
+    // extrinsics.rotate (Eigen::AngleAxisf (theta, Eigen::Vector3f::UnitZ()));
 
     // Print the transformation
     // printf ("\nMethod #2: using an Affine3f\n");
-    // std::cout << transform_2.matrix() << std::endl;
+    // std::cout << extrinsics.matrix() << std::endl;
+      // Executing the transformation
+    pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud (new pcl::PointCloud<pcl::PointXYZ> ());
+    // You can either apply transform_1 or extrinsics; they are the same
+    pcl::transformPointCloud (*bodyFiltered, *transformed_cloud, projection_matrix);
+
+    for (std::size_t i = 0; i < transformed_cloud->points.size (); ++i)
+    {
+        cout << "x:"<<transformed_cloud->points[i].x << endl;
+        cout << "y:"<<transformed_cloud->points[i].y << endl;
+        cout <<"z:"<< transformed_cloud->points[i].z << endl;
+    }
+
+    pcl::toROSMsg(*transformed_cloud,pub_cloud);
 
 
   } 
