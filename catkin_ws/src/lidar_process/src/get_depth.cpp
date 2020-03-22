e5b1f0c493a164fd975934e4c13362ff40d74d60
aacas <zhuzheyao7@gmail.com>
2020-03-21 22:42:54 -0400

    removed some useless code

--- catkin_ws/src/lidar_process/src/get_depth.cpp
+++ catkin_ws/src/lidar_process/src/get_depth.cpp
@@ -73,18 +73,6 @@ using namespace std;
 //     return 0;
 //   }
 
-void 
-setViewerPose (pcl::visualization::PCLVisualizer& viewer, const Eigen::Affine3f& viewer_pose)
-{
-  Eigen::Vector3f pos_vector = viewer_pose * Eigen::Vector3f(0, 0, 0);
-  Eigen::Vector3f look_at_vector = viewer_pose.rotation () * Eigen::Vector3f(0, 0, 1) + pos_vector;
-  Eigen::Vector3f up_vector = viewer_pose.rotation () * Eigen::Vector3f(0, -1, 0);
-  viewer.setCameraPosition (pos_vector[0], pos_vector[1], pos_vector[2],
-                            look_at_vector[0], look_at_vector[1], look_at_vector[2],
-                            up_vector[0], up_vector[1], up_vector[2]);
-}
-
-
 
 sensor_msgs::PointCloud2 pub_cloud;
 sensor_msgs::Image image_;
