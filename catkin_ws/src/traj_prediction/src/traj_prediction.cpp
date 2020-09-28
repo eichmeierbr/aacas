d78c5f6014566ee4270a8cea4ec12ac4f734f3ca
Stefan Zhu <zhuzheyao7@gmail.com>
2020-09-28 13:13:29 -0400

    added publisher

--- catkin_ws/src/traj_prediction/src/traj_prediction.cpp
+++ catkin_ws/src/traj_prediction/src/traj_prediction.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <lidar_process/tracked_obj.h>
 #include <lidar_process/tracked_obj_arr.h>
 // Cpp packages
 #include <stdlib.h>
@@ -12,8 +13,10 @@
 #include <unordered_map>
 #include<cmath>
 #include <fstream>
-#include <rviz_visual_tools/rviz_visual_tools.h>
+// #include <rviz_visual_tools/rviz_visual_tools.h>
 #include "geometry_msgs/Point.h"
+#include "Eigen/Dense"
+
 
 using namespace std;
 
@@ -27,13 +30,14 @@ struct instance_pos{
 
 class traj_predictor{
     private:
+    //key: object_id  value:object_label
     unordered_map<int, int> *obj_labels;
+    //key: object_id
     unordered_map<int, vector<pair<ros::Time, instance_pos>>>* obj_poses_dict; 
     ros::Subscriber tracked_obj_sub;
     ros::Publisher obj_trajectory_pub;
     // order of the polynomial used for prediction
     int pred_poly_order= 2;
-
     //seconds into the future that we want to predict
     int secs_into_future = 2;
     //the number of interpolated time stamps between now and the last future time stamp for prediction
@@ -47,6 +51,24 @@ class traj_predictor{
         this->obj_poses_dict = obj_poses_dict;
         this-> obj_labels = obj_labels;
         tracked_obj_sub = n.subscribe ("/tracked_obj_pos_arr", 10, &traj_predictor::tracked_obj_cb,this);
+        obj_trajectory_pub = n.advertise<lidar_process::tracked_obj_arr> ("obj_pos_pred_arr", 1);
+
+    }
+
+    void publisher( Eigen:: MatrixXf prediction, int obj_id){
+        lidar_process::tracked_obj_arr obj_pred_arr_msg;
+        for (int i =0; i < prediction.rows(); i++){
+            lidar_process::tracked_obj tracked_obj_msg;
+            geometry_msgs::Point point;
+            tracked_obj_msg.object_id = obj_id; 
+            point.x = prediction(i,0);
+            point.y = prediction(i,1);
+            point.z = prediction(i,2);
+            tracked_obj_msg.point = point;
+            tracked_obj_msg.header.stamp.sec=  prediction(i,3);
+            obj_pred_arr_msg.tracked_obj_arr.push_back(tracked_obj_msg);
+        }
+        obj_trajectory_pub.publish(obj_pred_arr_msg);
     }
 
     void predict_traj(){
@@ -55,6 +77,7 @@ class traj_predictor{
              // if it's a ball, then simply take the average. 
             if (x.first==0){
                 Eigen:: MatrixXf prediction = poly_predict(pred_poly_order, x.first);
+                publisher(prediction, x.first);
             }
             if ((*obj_labels)[x.first] == 0){
                 // if (x.first==0){
@@ -81,16 +104,16 @@ class traj_predictor{
 
 
     //make prediction using a polynomial function
-    Eigen:: MatrixXf poly_predict(int order, int obj_id){
+    Eigen::MatrixXf poly_predict(int order, int obj_id){
         int col_num = order+1;
         int row_num = (*obj_poses_dict)[obj_id].size();
-        Eigen:: MatrixXf T(row_num, col_num);
-        Eigen:: VectorXf X(row_num);
-        Eigen:: VectorXf Y(row_num);
-        Eigen:: VectorXf Z(row_num);
-        Eigen:: MatrixXf x_beta(row_num, 1);
-        Eigen:: MatrixXf y_beta(row_num, 1);
-        Eigen:: MatrixXf z_beta(row_num, 1);
+        Eigen::MatrixXf T(row_num, col_num);
+        Eigen::VectorXf X(row_num);
+        Eigen::VectorXf Y(row_num);
+        Eigen::VectorXf Z(row_num);
+        Eigen::MatrixXf x_beta(row_num, 1);
+        Eigen::MatrixXf y_beta(row_num, 1);
+        Eigen::MatrixXf z_beta(row_num, 1);
 
         double curr_time =ros::Time::now().toSec();
         for (int i = 0; i <(*obj_poses_dict)[obj_id].size(); i++){
@@ -110,29 +133,29 @@ class traj_predictor{
         z_beta = (T.transpose()*T).inverse()*T.transpose()*Z;
 
         //make the prediction
-        Eigen:: MatrixXf future_T(interlopated_pnts, col_num);
-        Eigen:: MatrixXf pred_T(interlopated_pnts,1);
+        Eigen::MatrixXf future_T(interlopated_pnts, col_num);
+        Eigen::MatrixXf T_pred(interlopated_pnts,1);
 
         for (int i =0 ; i < interlopated_pnts; i++){
             float time_stamp = (float)(secs_into_future/(float)interlopated_pnts)*(i+1); 
-            pred_T(i,0) = time_stamp + curr_time;
+            T_pred(i,0) = time_stamp;
             for (int j=0; j <= order ;j++){
                 future_T(i,j) = pow(time_stamp, j);
             }
         }
 
-        Eigen:: MatrixXf x_pred = future_T * x_beta;
-        Eigen:: MatrixXf y_pred = future_T * y_beta;
-        Eigen:: MatrixXf z_pred = future_T * z_beta;
+        Eigen::MatrixXf x_pred = future_T * x_beta;
+        Eigen::MatrixXf y_pred = future_T * y_beta;
+        Eigen::MatrixXf z_pred = future_T * z_beta;
 
-        Eigen:: MatrixXf pred(x_pred.rows(), x_pred.cols() + y_pred.cols() + z_pred.cols()+pred_T.cols());
+        Eigen::MatrixXf pred(x_pred.rows(), x_pred.cols() + y_pred.cols() + z_pred.cols()+T_pred.cols());
         // prediction << future_T * x_beta, future_T * y_beta, future_T * z_beta;
-        pred <<  x_pred,  y_pred,  z_pred , pred_T;
+        pred <<  x_pred,  y_pred,  z_pred , T_pred;
 
-        // Eigen:: MatrixXf pred_T = future_T.col(1);
+
         // cout << future_T.col(1) + curr_time << endl;
-        cout << "prediction" << endl;
-        cout << pred << endl;
+        // cout << "prediction" << endl;
+        // cout << pred << endl;
         // cout << "" << endl;
         return pred;
     }
@@ -151,9 +174,8 @@ class traj_predictor{
                     cout << "actual y position" << endl;
                     cout << obj.y << endl;
                 }
-
-
                 ////////////////////
+
                 if (!obj_poses_dict->count(it.object_id)){
                     vector<pair<ros::Time, instance_pos>> tmp_vec;
                     tmp_vec.push_back(make_pair(it.header.stamp,obj));
@@ -188,7 +210,7 @@ unordered_map<int, int> &obj_labels){
             if ((curr_time - it->first) > max_time_span){
                 x.second.erase(it);
                 // Delete the dictionary entry if the instance_pos vector is empty
-                if (x.second.size() <1){
+                if (x.second.size() < 1){
                     dead_keys.push_back(x.first);
                 }
             }
